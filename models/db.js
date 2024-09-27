e9ce6c575ee9a97aca30c17478243a497297af32
Ashish Basaula <basaulaashish73@gmail.com>
2024-09-27 12:30:47 +0545

    server hosting

--- models/db.js
+++ models/db.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql2');
 
-// const connection = mysql.createConnection({
-//   host: '95.216.194.29',  
-//   user: 'caring_hands',    
-//   password: 'ashish54321',  
-//   database: 'caring_hands',  
-//   port: 3306,
-//   connectTimeout:60000,               
-//   ssl: {
-//     rejectUnauthorized: false 
-//   }
-// });
-
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // your MySQL root password if any
-  database: 'caring_hands'
+  host: '95.216.194.29',  
+  user: 'caring_hands',    
+  password: 'ashish54321',  
+  database: 'caring_hands',  
+  port: 3306,
+  connectTimeout:60000,               
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '', // your MySQL root password if any
+//   database: 'caring_hands'
+// });
 
 
 // Connect to the database
